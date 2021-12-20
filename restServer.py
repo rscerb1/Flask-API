2df7942f1149d77da2fee6e380908d9f40de0ce7
reggie <rscerb1@students.towson.edu>
2021-12-20 03:47:37 -0500

    added logger to api

--- restServer.py
+++ restServer.py
@@ -2,7 +2,8 @@ from flask import Flask, request
 from flask_restful import reqparse, Api, Resource
 from waitress import serve
 from checkBoard import Board
-import sqlFunctions
+import sqlFunctions, datetime
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -20,27 +21,42 @@ parser.add_argument('password')
 # USER
 #
 
+# error logger
+def log(message):
+    with open('log.txt', 'a+') as logFile:
+        logFile.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\t'API:'\t{message}\n")
+
+
 class Players(Resource):
     # return specified user
     def get(self):
-        args = request.args.to_dict()
-        if(len(args) == 0):
-            # return all users
-            return sqlFunctions.getUsers()
-        if(len(args) == 1):
-            # return a specified user
-            return sqlFunctions.getUser(args['username'])
-        else:
-            return "Invalid argument(s)"
+        try:
+            args = request.args.to_dict()
+            if(len(args) == 0):
+                # return all users
+                return sqlFunctions.getUsers()
+            if(len(args) == 1):
+                # return a specified user
+                return sqlFunctions.getUser(args['username'])
+            else:
+                return "Invalid argument(s)"
+        except:
+            log('Error getting player(s)')
         
     # add new user
     def post(self):
-        args = parser.parse_args()
-        return sqlFunctions.postUser(parser.parse_args()['username'],parser.parse_args()['password']), 201
+        try:
+            args = parser.parse_args()
+            return sqlFunctions.postUser(parser.parse_args()['username'],parser.parse_args()['password']), 201
+        except:
+            log('Error posting player')
     
     # delete a specified user
     def delete(self):
-        return sqlFunctions.deleteUser(parser.parse_args()['user']), 204
+        try:
+            return sqlFunctions.deleteUser(parser.parse_args()['user']), 204
+        except:
+            log('Error deleting player')
     
 #
 # GAME
@@ -48,34 +64,46 @@ class Players(Resource):
 
 class Game(Resource):
     def get(self):
-        args = request.args.to_dict()
-        if(len(args) == 0):
-            # return all games
-            return sqlFunctions.getGames()
-        if(len(args) == 1):
-            # return all games of specified user
-            return sqlFunctions.getUserGames(str(args['username']))
-        if(len(args) == 2):
-            # return a game between 2 specified players
-            return sqlFunctions.getGame(str(args['player0']), str(args['player1']))
-        else:
-            return "Invalid argument(s)"
+        try:
+            args = request.args.to_dict()
+            if(len(args) == 0):
+                # return all games
+                return sqlFunctions.getGames()
+            if(len(args) == 1):
+                # return all games of specified user
+                return sqlFunctions.getUserGames(str(args['username']))
+            if(len(args) == 2):
+                # return a game between 2 specified players
+                return sqlFunctions.getGame(str(args['player0']), str(args['player1']))
+            else:
+                return "Invalid argument(s)"
+        except:
+            log('Error getting game(s)')
     
     # create new game between 2 users
     def post(self):
-        args = parser.parse_args()
-        return sqlFunctions.postGame(args['player0'], args['player1']), 201
+        try:
+            args = parser.parse_args()
+            return sqlFunctions.postGame(args['player0'], args['player1']), 201
+        except:
+            log('Error posting game')
     
     # update a game between 2 users
     def put(self):
-        args = parser.parse_args()
-        board = Board(args['board'])
-        return sqlFunctions.putGame(args['player0'], args['player1'], args['board'], args['turn'], board.getStatus()), 201
+        try:
+            args = parser.parse_args()
+            board = Board(args['board'])
+            return sqlFunctions.putGame(args['player0'], args['player1'], args['board'], args['turn'], board.getStatus()), 201
+        except:
+            log('Error updating player')
     
     # delete a game between 2 users
     def delete(self):
-        args = parser.parse_args()
-        return sqlFunctions.deleteGame(args['player0'], args['player1']), 204
+        try:
+            args = parser.parse_args()
+            return sqlFunctions.deleteGame(args['player0'], args['player1']), 204
+        except:
+            log('Error deleting game')
     
 # addresses
 api.add_resource(Players, '/players')
